cce5a6a73b0ef3eb03ada735ef0957f1681c2854
scruz91 <pscn91@gmail.com>
2020-02-06 12:32:52 -0300

    Updates

---
+++ precache-manifest.5c6b7b09ac8a96e488e189162e0f4c09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3be51544296265ea312de444a3fb18a9",
+    "url": "/supervisor/index.html"
+  },
+  {
+    "revision": "20e3a83529006c45b076",
+    "url": "/supervisor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7daa45b31c29043d0fbe",
+    "url": "/supervisor/static/js/2.07e219b4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/supervisor/static/js/2.07e219b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20e3a83529006c45b076",
+    "url": "/supervisor/static/js/main.60e109a2.chunk.js"
+  },
+  {
+    "revision": "6b8208360ff773b01638",
+    "url": "/supervisor/static/js/runtime-main.09acce18.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/supervisor/static/media/logo.5d5d9eef.svg"
+  }
+]);
